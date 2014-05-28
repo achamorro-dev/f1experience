c168375abd9d938708752730c872ab952dea368f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2014-05-28 12:19:11 +0200

    Added index url direct to template

--- f1experience/urls.py
+++ f1experience/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+#from django.contrib import admin
 
-from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'f1experience.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    #url(r'^admin/', include(admin.site.urls)),
 )
